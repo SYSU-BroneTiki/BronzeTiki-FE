ba9ee7a29447b0d553f26e08dd13ac064515b8e1
xiezht <xiewangchuang@outlook.com>
2018-05-09 14:31:32 +0800

    创建首页头部子组件

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="header-left">header-left</div>
+    <div class="header-title">header-title</div>
+    <div class="header-right">header-right</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  // html font-size = 50px = 1rem
+  .header
+    height: 1.8rem
+    background: #005DFB
+    .header-left
+      background: red
+    .header-title
+      background: yellow
+    .header-right
+      background: gray
+</style>
