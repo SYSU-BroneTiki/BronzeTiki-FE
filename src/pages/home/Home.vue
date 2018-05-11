be57385a9e053e3d35da28da120280a384354540
xiezht <xiewangchuang@outlook.com>
2018-05-12 00:29:48 +0800

    首页顶部重新布局

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="home">
-    <el-container>
-      <el-header>
-        <home-header></home-header>
-      </el-header>
-      <el-main>
-        <home-list></home-list>
-      </el-main>
-    </el-container>
+    <home-header></home-header>
+    <home-list></home-list>
   </div>
 </template>
 
@@ -24,14 +18,5 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .home
-    background: #E3EBF7
-  .el-header
-    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
-    color: #fff
-    padding: 0
-    margin-bottom: 0.8rem
-  .el-main
-    padding: 0.1rem
-    overflow: visible
+
 </style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
-  <el-row class="header-row">
-    <el-col class="header-left" :span="4">
-      <i class="el-icon-back"></i>
-    </el-col>
-    <el-col class="header-right" :span="4" :offset="16">
-      <i class="el-icon-search"></i>
-    </el-col>
-  </el-row>
+  <div class="header">
+    <el-row class="header-row">
+      <el-col class="header-left" :span="4">
+        <i class="el-icon-back"></i>
+      </el-col>
+      <el-col class="header-right" :span="4" :offset="16">
+        <i class="el-icon-search"></i>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -18,11 +20,16 @@ export default {
 <!-- html font-size = 50px = 1rem -->
 <style lang="stylus" scoped>
   @import '../../../assets/variables.styl'
-  .header-row
+  .header
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif
     background: $bgColor
+    color: #fff
     font-size: 0.4rem
-    line-height: 0.6rem
     text-align: center
-    padding-top: 0.5rem
-    padding-bottom: 0.2rem
+    line-height: 1.0rem
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    z-index: 1
 </style>
