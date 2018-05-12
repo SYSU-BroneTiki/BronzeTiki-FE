69eb0fb6f2f82df69d71275b18c33afe029f158a
xiezht <xiezhd@mail2.sysu.edu.cn>
2018-05-12 21:48:01 +0800

    增加底部导航栏

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <router-link to="/home">
+      <span class="iconfont icon-left">&#xe62e;</span>
+    </router-link>
+    <router-link to="/">
+      <span class="iconfont icon-right">&#xe62f;</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomNav'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    color: #000
+    font-size: 0
+    .icon-left
+    .icon-right
+      display: inline-block
+      width: 50%
+      color: #000
+      text-align: center
+      font-size: 1rem
+      line-height: 1.2rem
+</style>
