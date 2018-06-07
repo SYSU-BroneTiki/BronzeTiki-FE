55eb6005d8b0166df13becb6f264da1f47a5a387
xiezht <xiezhd@mail2.sysu.edu.cn>
2018-06-07 15:33:58 +0800

    更新Screen组件

---
+++ src/pages/home/components/Screen.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="item-content">
+    <div>日期</div>
+    <div class="date-selection">
+      <el-button type="primary" round>Today</el-button>
+      <el-button type="primary" round>Tomorrow</el-button>
+      <el-button type="primary" round>Today</el-button>
+      <el-button type="primary" round>Tomorrow</el-button>
+      <el-button type="primary" round>Today</el-button>
+      <el-button type="primary" round>Tomorrow</el-button>
+      <el-button type="primary" round>Today</el-button>
+      <el-button type="primary" round>Tomorrow</el-button>
+    </div>
+    <div>场次</div>
+    <div class="time-selection">
+      <el-button type="primary" round>8:00</el-button>
+      <el-button type="primary" round>12:00</el-button>
+      <el-button type="primary" round>19:00</el-button>
+    </div>
+    <div style="text-align: center">
+      <el-button type="danger" round>确认</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Screen'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-content
+      font-size: 0.3rem
+      font-weight: bold
+      margin-left: 0.2rem
+      .date-selection
+      .time-selection
+        display:flex !important
+        overflow-x: scroll
+        margin-bottom: 0.1rem
+</style>
