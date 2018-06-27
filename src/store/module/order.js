05262ad9bac6870738bdf1b2ad1378d2b1bd7dea
xiezht <xiezhd@mail2.sysu.edu.cn>
2018-06-27 21:23:51 +0800

    更新store

--- src/store/module/order.js
+++ src/store/module/order.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+// import DataProcess from '../../common/utils/DataProcess'
 
 export default {
   state: {
@@ -6,15 +7,26 @@ export default {
   },
   mutations: {
     SET_ORDER (state, order) {
-      state.orderInfo = order.data
-      console.log('SET_ORDER_SUCC', order.data)
+      state.orderInfo = order
     }
   },
   actions: {
     CREATE_ORDER ({ commit }, payload) {
-      return axios.get('/static/mock/order-confirm.json', payload).then(res => {
+      // console.log('POST数据', tOrder)
+      console.log('POST订单数据', payload)
+      return axios.post('/test/api/orders', payload).then(res => {
         if (res.status === 200) {
+          console.log('订单提交返回数据', res.data)
           commit('SET_ORDER', res.data)
+        }
+      })
+    },
+    PAY_ORDER ({ commit }, payload) {
+      // return axios.patch('/test/api/orders/' + payload.orderId).then(res => {
+      return axios.get('/static/mock/pay-result.json').then(res => {
+        console.log(res)
+        if (res.status === 200) {
+          return res.data
         }
       })
     }
